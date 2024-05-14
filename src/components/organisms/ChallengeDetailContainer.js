08365e9af6f67dc0cc8b9521539bb9c947655cc2
yang <junsuk530@gmail.com>
2024-05-14 19:18:59 +0900

    챌린지 목록 헤더 완성

---
+++ src/components/organisms/ChallengeDetailContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChallengeDetailContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChallengeDetailContainer;
