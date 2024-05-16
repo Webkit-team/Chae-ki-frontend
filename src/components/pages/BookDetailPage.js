93773e98463b011cebff7bcb8d35130d084bfcce
ohtaesiki-dolawatguna <potentialI510@gmail.com>
2024-05-16 22:40:04 +0900

    [도서상세페이지] 초안 작성

---
+++ src/components/pages/BookDetailPage.js
@@ -0,0 +1,13 @@
+import Layout from "../Layout/Layout";
+import BookContainer from "../organisms/BookContainer";
+
+const BookDetailPage = () => {
+    return (<>
+        <Layout>
+            <BookContainer/>
+        </Layout>
+    
+    </>)
+}
+
+export default BookDetailPage;
