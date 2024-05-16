55aec83ff134f110336eca8a729af8bb571939b1
yang <junsuk530@gmail.com>
2024-05-16 21:59:05 +0900

    dayjs 라이브러리 설치

--- src/components/molecules/ChallengeDetail/ChaekiTime.js
+++ src/components/molecules/ChallengeDetail/ChaekiTime.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Text4 } from '../../atoms/Text';
 
 const ChaekiTime = () => {
     return (
-        <div>
-            채키타임 컴포넌트
-        </div>
+        <>
+            <Text4 sx={{fontFamily:'DS-DIGI', fontSize:'100px'}}>15:00</Text4>
+        </>
     );
 };
 
