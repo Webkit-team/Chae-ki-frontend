5d1d8e8e2947e5399b3c897ad79a13d52ed6a77a
yang <junsuk530@gmail.com>
2024-05-24 14:46:19 +0900

    파이어베이스 호스팅 완료

--- src/components/organisms/ReviewContainer.js
+++ src/components/organisms/ReviewContainer.js
@@ -1,6 +1,15 @@
+import { Box } from "@mui/material";
+import png02 from '../../assets/png02.png';
+import { SubTitle } from "../atoms/Text";
+
 const ReviewContainer = () => {
     return (
-        <h1>독서 후기 페이지입니다.</h1>
+        <>
+            <SubTitle>도서 후기</SubTitle>
+            <Box sx={{ width: '100%', hegiht: '100%' }}>
+                <img src={png02} alt="개발 진행중" style={{marginLeft:'27px'}}/>
+            </Box>
+        </>
     );
 }
 
